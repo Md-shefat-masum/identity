993a5484909e00170f5f0a7206329a9d83c07996
Md-shefat-masum <myphoto240@gmail.com>
2019-07-22 05:09:20 -0600

    add resume database

---
+++ database/migrations/2019_07_22_164935_create_myresumes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMyresumesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('myresumes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('section_name',100)->nullable();
+            $table->string('course_heading',100)->nullable();
+            $table->string('course_sub_heading',100)->nullable();
+            $table->text('course_details');
+            $table->string('start_date',100)->nullable();
+            $table->string('end_date',100)->nullable();
+            $table->string('slug',35)->default('sdfdsfvdff');
+            $table->integer('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('myresumes');
+    }
+}
