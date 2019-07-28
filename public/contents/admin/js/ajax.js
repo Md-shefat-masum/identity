3494eb9a3c0584c4163f22bf1ab871285817a051
Md-shefat-masum <myphoto240@gmail.com>
2019-07-29 02:11:39 +0600

    --- Auto-Git Commit ---

--- public/contents/admin/js/ajax.js
+++ public/contents/admin/js/ajax.js
@@ -4,11 +4,26 @@ $(document).ready(function(){
         $('#name').html( $(this).data('name'));
         $('#name2').html( $(this).data('name'));
         $('#email').html( $(this).data('email'));
-        $('#role').html( $(this).data('name'));
+        $('#role').html( $(this).data('role'));
         $('#role2').html( $(this).data('role'));
         $('#creator').html( $(this).data('creator'));
-        $('#photo').attr('src'," {{asset('')}}"+"{{"+$(this).data('photo')+"}}");
+        // $('#photo').attr('src'," {{asset('')}}"+"{{"+$(this).data('photo')+"}}");
         // console.log('{{asset("")}}'+'{{'+$(this).data('photo')+'}}');
+        $.ajax({
+            type: 'GET',
+            url: '/admin/view-user-modal',
+            dataType:'json',
+            success: function(data) {
+                // $('#single-data').modal('show');
+                // ... the other stuff
+                $('#photo').attr('src', data.photo);
+            },
+            error : function() {
+                alert("Ghorar DIm");
+            }
+        });
     });
+
+
 
 });
