3eaac451ad9d3d91d09d24cf4dc366e1196d18a0
Md-shefat-masum <myphoto240@gmail.com>
2019-07-29 03:11:52 +0600

    --- Auto-Git Commit ---

--- public/contents/admin/js/ajax.js
+++ public/contents/admin/js/ajax.js
@@ -1,23 +1,23 @@
 
 $(document).ready(function(){
     $('.view-modal').click(function() {
-        $('#name').html( $(this).data('name'));
-        $('#name2').html( $(this).data('name'));
-        $('#email').html( $(this).data('email'));
         $('#role').html( $(this).data('role'));
         $('#role2').html( $(this).data('role'));
-        $('#creator').html( $(this).data('creator'));
         // $('#photo').attr('src'," {{asset('')}}"+"{{"+$(this).data('photo')+"}}");
         // console.log('{{asset("")}}'+'{{'+$(this).data('photo')+'}}');
-        console.log($(this).data('id'));
+        // console.log($(this).data('id'));
         $.ajax({
             type: 'GET',
             url: $(this).data('id'),
             dataType:'json',
             success: function(data) {
-                // $('#single-data').modal('show');
+                $('.view-modal').show();
                 // ... the other stuff
-                $('#photo').attr('src',"afas;ldjf");
+                $('#photo').attr('src',"{{asset('')}}{{$data->photo}}");
+                $('#name').html( data.name );
+                $('#name2').html( data.name );
+                $('#email').html( data.email);
+                $('#creator').html( data.creator);
             },
             error : function(data) {
                 alert(data.name);
